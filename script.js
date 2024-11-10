99ce607bf425303f78bbc7d3373f1096b30a57de
d1nd1nok <166098023+d1nd1nok@users.noreply.github.com>
2024-11-10 21:33:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var names = ["John", "Sara", "James", "Michael", "Rachel", "Jill"];
+
+for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    
+    // Check if the name starts with 'J' or 'j'
+    if (name.charAt(0).toLowerCase() === 'j') {
+        console.log("Goodbye " + name);
+    } else {
+        console.log("Hello " + name);
+    }
+}
 
